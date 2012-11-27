959f3c72ecaaafc4168b7d3dc404810f8ea670e3
akima <akm2000@gmail.com>
2012-11-27 19:07:09 +0900

    [運用者が手動でHadoopを使った集計処理を実行できる] デバッグ用のログを出力しないようにしました

--- serde/src/java/com/groovenauts/hive/mongo/SqlTimestamp.java
+++ serde/src/java/com/groovenauts/hive/mongo/SqlTimestamp.java
@@ -4,18 +4,14 @@ import java.sql.Timestamp;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 public class SqlTimestamp {
 	
 	public static final Pattern DATE_TIME_ISO8601_T = Pattern.compile("T");
 	public static final Pattern DATE_TIME_ISO8601_Z = Pattern.compile("Z$");
 
-    private static final Log LOG = LogFactory.getLog(SqlTimestamp.class);
+    // private static final Log LOG = LogFactory.getLog(SqlTimestamp.class);
 
 	public static Timestamp valueOf(String s){
-		LOG.warn("SqlTimestamp is working!!");
 		final Matcher mt = DATE_TIME_ISO8601_T.matcher(s);
 		s = mt.replaceFirst(" ");
 		final Matcher mz = DATE_TIME_ISO8601_Z.matcher(s);
